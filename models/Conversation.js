b7546cf5538f31f22808331c51413dfd446149d5
lyula <sacredlyula@gmail.com>
2025-06-26 02:36:52 +0300

    removed the conversation model

--- models/Conversation.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ConversationSchema = new mongoose.Schema({
-  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // The other user in the conversation
-  lastMessage: { type: mongoose.Schema.Types.ObjectId, ref: "Message" },
-}, { timestamps: true });
-
-module.exports = mongoose.model("Conversation", ConversationSchema);
