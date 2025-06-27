c0660c48f9707a5aa14fd1912786ab7d751b0e99
lyula <sacredlyula@gmail.com>
2025-06-27 19:02:44 +0300

    fteching posts on the profile page using profile user _id of the user

--- routes/post.js
+++ routes/post.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { createPost, getPosts, likePost, addComment } = require("../controllers/postController");
 const auth = require("../middleware/auth");
+const Post = require("../models/Post");
+const User = require("../models/User");
 
 // Create a new post
 router.post("/", auth, createPost);
@@ -18,13 +20,21 @@ router.post("/:postId/comment", auth, addComment);
 // Get posts by username (public profile)
 router.get("/user/:username", async (req, res) => {
   try {
-    const User = require("../models/User");
-    const Post = require("../models/Post");
     const user = await User.findOne({ username: req.params.username });
     if (!user) return res.status(404).json({ error: "User not found" });
 
     // Find posts where author matches user's _id
     const posts = await Post.find({ author: user._id }).populate("author", "username verified");
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch user's posts" });
+  }
+});
+
+// Get posts by user ID (robust to username changes)
+router.get("/by-userid/:userId", async (req, res) => {
+  try {
+    const posts = await Post.find({ author: req.params.userId }).populate("author", "username verified");
     res.status(200).json(posts);
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch user's posts" });
