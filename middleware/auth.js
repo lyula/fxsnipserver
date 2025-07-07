fcd0ca4ec1284cefa305a4512165fdd9ea0b9773
lyula <sacredlyula@gmail.com>
2025-07-07 18:29:27 +0300

    added better logs to see where the issue is

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,10 @@
 const jwt = require("jsonwebtoken");
 
 function requireAuth(req, res, next) {
+  console.log("AUTH HEADERS:", req.headers); // <-- Added this
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    console.log("No token provided");
     return res.status(401).json({ message: "No token provided" });
   }
   const token = authHeader.split(" ")[1];
@@ -11,6 +13,7 @@ function requireAuth(req, res, next) {
     req.user = decoded;
     next();
   } catch (err) {
+    console.log("JWT error:", err); // <-- Added this
     return res.status(401).json({ message: "Invalid token" });
   }
 }
