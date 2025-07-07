8d98e40789cc7f453ea60d09c11a8cddc01a9e81
lyula <sacredlyula@gmail.com>
2025-07-07 21:20:58 +0300

    fixed the routes to use the requireAuth

--- routes/badgePayment.js
+++ routes/badgePayment.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { createBadgePayment, initiateSTKPush, payheroCallback } = require('../controllers/badgePaymentController');
+const requireAuth = require("../middleware/auth"); // Add auth middleware
 
 // Create a badge payment
-router.post('/', createBadgePayment);
+router.post('/', requireAuth, createBadgePayment);
 
 // Initiate PayHero STK Push
-router.post('/initiate-stk', initiateSTKPush);
+router.post('/initiate-stk', requireAuth, initiateSTKPush);
 
 // PayHero callback endpoint
 router.post('/payhero-callback', payheroCallback);
