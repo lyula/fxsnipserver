fd93e4d9443e86d29985f73ef53bb552b7ff1eed
lyula <sacredlyula@gmail.com>
2025-07-09 23:58:33 +0300

    fixing dual visibility of online status whenever someone is on any page of the app

--- sockets/online.js
+++ sockets/online.js
@@ -20,6 +20,12 @@ module.exports = function onlineSocket(io, socket, onlineUsers) {
     }
   });
 
+  // Send current online users to the requesting client
+  socket.on("get-online-users", () => {
+    const onlineUserIds = Object.keys(onlineUsers).filter(userId => onlineUsers[userId] && onlineUsers[userId].length > 0);
+    socket.emit("online-users-list", { userIds: onlineUserIds });
+  });
+
   // Disconnect event
   socket.on("disconnect", async () => {
     if (socket.userId && onlineUsers[socket.userId]) {
