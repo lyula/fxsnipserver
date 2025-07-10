9c2f28a5de5fceb5992ba31602db2c7dd10858e1
lyula <sacredlyula@gmail.com>
2025-07-10 06:35:03 +0300

    removed the duplicate typing code in separate files, now only in messaging,js

--- sockets/online.js
+++ sockets/online.js
@@ -1,25 +1,7 @@
-// Handles online status, typing, and last seen events for socket.io
+// Handles online status and last seen events for socket.io
 const User = require("../models/User");
 
 module.exports = function onlineSocket(io, socket, onlineUsers) {
-  // Typing event
-  socket.on("typing", (data) => {
-    if (data?.toUserId && onlineUsers[data.toUserId] && onlineUsers[data.toUserId].length > 0) {
-      onlineUsers[data.toUserId].forEach(socketId => {
-        io.to(socketId).emit("typing", { fromUserId: socket.userId });
-      });
-    }
-  });
-
-  // Stop typing event
-  socket.on("stop-typing", (data) => {
-    if (data?.toUserId && onlineUsers[data.toUserId] && onlineUsers[data.toUserId].length > 0) {
-      onlineUsers[data.toUserId].forEach(socketId => {
-        io.to(socketId).emit("stop-typing", { fromUserId: socket.userId });
-      });
-    }
-  });
-
   // Send current online users to the requesting client
   socket.on("get-online-users", () => {
     const onlineUserIds = Object.keys(onlineUsers).filter(userId => onlineUsers[userId] && onlineUsers[userId].length > 0);
