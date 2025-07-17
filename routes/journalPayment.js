0bc8c6578e9a1d8ff742c81f8b6606c353a25f0b
lyula <sacredlyula@gmail.com>
2025-07-17 21:52:13 +0300

    fixing persisting errors

--- routes/journalPayment.js
+++ routes/journalPayment.js
@@ -19,21 +19,3 @@ router.get('/', auth, journalPaymentController.getAllJournalPayments);
 router.get('/status', auth, journalPaymentController.getJournalPaymentStatus);
 
 module.exports = router;
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/auth');
-const journalPaymentController = require('../controllers/journalPaymentController');
-
-// Create payment and initiate STK push
-router.post('/', auth, journalPaymentController.createJournalPayment);
-
-// Payhero callback
-router.post('/payhero-callback', journalPaymentController.payheroCallback);
-
-// Get latest payment for user
-router.get('/latest', auth, journalPaymentController.getLatestJournalPayment);
-
-// Get all payments for user
-router.get('/', auth, journalPaymentController.getAllJournalPayments);
-
-module.exports = router;
