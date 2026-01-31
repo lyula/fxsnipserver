0739eb7eee98f5909d66be204b1f5b856ff412f0
lyula <sacredlyula@gmail.com>
2026-01-31 18:49:21 +0300

    added the cors origin to allow new domain name

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const allowedOrigins = [
   process.env.CLIENT_URL,
   'http://localhost:5173',
   'https://journalzye-plc.vercel.app',
-  'http://127.0.0.1:5173',
+  'http://tradewall.vercel.app',
   'http://localhost:8080',
   'http://127.0.0.1:8080',
   'http://localhost:8081', // React Native Metro
